78e0b2157d60cd6d7a989d438e078ce5efcda3ef
Eden Eltume <IEltume@gmail.com>
2015-12-13 00:20:30 -0500

    Modeling and Displaying posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
@@ -12,8 +13,11 @@ class PagesController < ApplicationController
     else
       redirect_to root_path, :notice=> "User not found!"
     end
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
   end
 
   def explore
+    @posts = Post.all
   end
+
 end
